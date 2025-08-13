262c105b9de1f8cc8e530183c33cdd1b2f651e2d
PC <winniewaiguru@gmail.com>
2025-08-13 21:46:18 +0300

    Navigation bar styling

--- my-book-app/src/components/Header.jsx
+++ my-book-app/src/components/Header.jsx
@@ -1,25 +1,27 @@
 function Header() {
   return (
-    <header className="p-4">
-      <nav>
+    <header className="md:py-4 md:px-12 ">
+      <nav className="h-20 flex flex-col md:flex-row justify-between items-center">
         <div>
-          <img
-            className="w-24 h-12"
-            src="\public\Logo.png"
-            alt="bookapp-logo"
-          />
+          <img className="w-24 h-12" src="\Logo.png" alt="bookapp-logo" />
         </div>
-        <ul>
-          <li>
-            <a href="">Home</a>
-          </li>
-          <li>
-            <a href="">Favorites</a>
-          </li>
-          <li>
-            <a href="">Download List</a>
-          </li>
-        </ul>
+        <div>
+          <ul className=" font-bold text-base flex flex-col md:flex-row gap-24 h-full md:h-10 items-center bg-amber-300 ">
+            <li className="nav-items">
+              <a href="">Home</a>
+            </li>
+            <li className="nav-items">
+              <a href="">Favorites</a>
+            </li>
+            <li className="nav-items">
+              <a href="">Download List</a>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <i class="fa-solid fa-bars icon nav-items"></i>
+        </div>
       </nav>
     </header>
   );
