640fa43311cbe4605b6da2e68a1acf5990fbd3ba
PC <winniewaiguru@gmail.com>
2025-08-14 16:48:13 +0300

    Adding responsiveness to the header

--- my-book-app/src/components/Header.jsx
+++ my-book-app/src/components/Header.jsx
@@ -1,26 +1,65 @@
-function Header() {
-  return (
-    <header className="md:py-4 md:px-12 ">
-      <nav className="h-20 flex flex-col md:flex-row justify-between items-center">
-        <div>
-          <img className="w-24 h-12" src="\Logo.png" alt="bookapp-logo" />
-        </div>
-        <div>
-          <ul className=" font-bold text-base flex flex-col md:flex-row gap-24 h-full md:h-10 items-center bg-amber-300 ">
-            <li className="nav-items">
-              <a href="">Home</a>
-            </li>
-            <li className="nav-items">
-              <a href="">Favorites</a>
-            </li>
-            <li className="nav-items">
-              <a href="">Download List</a>
-            </li>
-          </ul>
-        </div>
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
+import { FaXmark } from "react-icons/fa6";
 
-        <div>
-          <i class="fa-solid fa-bars icon nav-items"></i>
+function Header() {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <header
+      className={`${
+        isActive
+          ? "w-[300px] bg-[#F7FCFF] rounded-tr-4xl"
+          : "w-full bg-transparent"
+      } relative`}
+    >
+      <nav className=" px-6 py-4  h-16 flex justify-between items-center">
+        <img
+          className={`w-24 h-12 ${isActive && "hidden"} md:block`}
+          src="\Logo.png"
+          alt="bookapp-logo"
+        />
+
+        {/* Desktop Menu */}
+
+        <ul className=" hidden font-bold md:flex flex-row gap-20 text-xl px-10">
+          <li className="nav-items">
+            <a href="">Home</a>
+          </li>
+          <li className="nav-items">
+            <a href="">Favorites</a>
+          </li>
+          <li className="nav-items">
+            <a href="">Download List</a>
+          </li>
+        </ul>
+
+        {/* Mobile View menu */}
+        {isActive && (
+          <div className="absolute top-[64px] left-0 bg-[#F7FCFF] w-[300px] font-bold ">
+            <ul className="flex flex-col gap-6 h-screen items-center justify-evenly ">
+              <li className="sideItems-hover">
+                <a href="">Home</a>
+              </li>
+              <li className="sideItems-hover">
+                <a href="">Favorites</a>
+              </li>
+              <li className="sideItems-hover">
+                <a href="">Download List</a>
+              </li>
+            </ul>
+          </div>
+        )}
+
+        <div className="md:hidden">
+          {!isActive ? (
+            <FaBars className="icon" onClick={() => setIsActive(true)} />
+          ) : (
+            <FaXmark
+              className="icon absolute right-10"
+              onClick={() => setIsActive(false)}
+            />
+          )}
         </div>
       </nav>
     </header>
